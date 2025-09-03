84fd59f01ee2f93af6184d6dd6cca8af406fedc3
zhanghui21 <rzxyxxzhx1103@gmail.com>
2025-09-03 15:46:16 -0500

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
+    // @ts-expect-error
+    transpilePackages: ['lib/editor'],
     ppr: true,
   },
   images: {
