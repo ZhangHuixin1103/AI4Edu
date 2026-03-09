5e0da268e577123f178f256a9e72690b1e80a53f
zhaoming0812 <zhaoming0812@csce-tzz-s1.engr.tamu.edu>
2026-03-08 22:46:36 -0500

    udpate UI

--- lib/ai/entitlements.ts
+++ lib/ai/entitlements.ts
@@ -8,11 +8,7 @@ interface Entitlements {
 
 export const entitlementsByUserType: Record<UserType, Entitlements> = {
   defaultType: {
-<<<<<<< Updated upstream
     maxMessagesPerDay: Infinity,
-=======
-    maxMessagesPerDay: 10,
->>>>>>> Stashed changes
     availableChatModelIds: ['Llama-3.1-Math'],
   },
 
@@ -20,11 +16,7 @@ export const entitlementsByUserType: Record<UserType, Entitlements> = {
    * For users without an account
    */
   guest: {
-<<<<<<< Updated upstream
     maxMessagesPerDay: Infinity,
-=======
-    maxMessagesPerDay: 20,
->>>>>>> Stashed changes
     availableChatModelIds: ['Llama-3.1-Math'],
   },
 
@@ -32,11 +24,7 @@ export const entitlementsByUserType: Record<UserType, Entitlements> = {
    * For users with an account
    */
   regular: {
-<<<<<<< Updated upstream
     maxMessagesPerDay: Infinity,
-=======
-    maxMessagesPerDay: 100,
->>>>>>> Stashed changes
     availableChatModelIds: ['Llama-3.1-Math'],
   },
 
